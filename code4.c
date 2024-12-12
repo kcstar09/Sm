fecb17fbec390e7b03fa5f888c044b333e038180
kcstar09 <kc791534@gmail.com>
2024-12-12 08:07:18 +0530

    Update code4.c

--- code4.c
+++ code4.c
@@ -1,42 +1,43 @@
 #include<stdio.h>
-#define INF 999
-void dijkstra(int c[10][10],int n,int s,int d[10])
+int rem(int,int);
+void main()
 {
-    int v[10],min,u,i,j;
-    for(i=1; i<=n; i++)
-    {
-        d[i]=c[s][i];
-        v[i]=0;
-    }
-    v[s]=1;
-    for(i=1; i<=n; i++)
-    {
-        min=INF;
-        for(j=1; j<=n; j++)
-            if(v[j]==0 && d[j]<min)
-            {
-                min=d[j];
-                u=j;
-            }
-        v[u]=1;
-        for(j=1; j<=n; j++)
-            if(v[j]==0 && (d[u]+c[u][j])<d[j])
-                d[j]=d[u]+c[u][j];
-    }
-}
-int main()
+int i,j,k,d,dl;
+int data[10],div[5],newdata[15],crc[5],datacrc[15],revdata[15],remd[5];
+printf("\n Enter the data length= ");
+scanf("%d",&d);
+printf("\n Enter the divisor length= ");
+scanf("%d",&dl);
+printf("\n Enter the data : ");
+for(i=0;i<d;i++)
+scanf("%d",&data[i]);
+printf("\n Enter the divisor : ");
+for(i=0;i<dl;i++)
+scanf("%d",&div[i]);
+printf("\n The new data is : ");
+for(i=0;i<(d+dl-1);i++)
 {
-    int c[10][10],d[10],i,j,s,sum,n;
-    printf("\nEnter n value:");
-    scanf("%d",&n);
-    printf("\nEnter the graph data:\n");
-    for(i=1; i<=n; i++)
-        for(j=1; j<=n; j++)
-            scanf("%d",&c[i][j]);
-    printf("\nEnter the souce node:");
-    scanf("%d",&s);
-    dijkstra(c,n,s,d);
-    for(i=1; i<=n; i++)
-        printf("\nShortest distance from %d to %d is %d",s,i,d[i]);
-    return 0;
+if(i<d)
+newdata[i]=data[i];
+else
+newdata[i]=0;
+printf("%d",newdata[i]);
 }
+for(j=0;j<=d;j++)
+{
+for(i=0;i<dl;i++)
+{
+crc[i]=newdata[i+j];
+if(crc[0]==1)
+newdata[i+j]=rem(newdata[i+j],div[i]);
+else
+newdata[i+j]=rem(newdata[i+j],0);
+}
+printf("\n The Crc is : ");
+for(i=0;i<dl-1;i++)
+printf("%d",crc[i]);
+}
+printf("\n The data to be send is : ");
+for(i=0;i<(d+dl-1);i++)
+{
+if(i<d)
