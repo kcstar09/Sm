233c3b730caab3cbda661124b0baaef5798a421d
kcstar09 <kc791534@gmail.com>
2024-12-12 08:00:16 +0530

    Update code6.c

--- code6.c
+++ code6.c
@@ -1,27 +1,35 @@
 #include<stdio.h>
-int w[10], p[10],n;
-int max(int a,int b)
-{
-return a>b?a:b;
-}
-int knap(int i,int m)
-{
-if(i==n) return w[i]>m?0:p[i];
-if(w[i]>m) return knap(i+1,m);
-return
-max(knap(i+1,m),knap(i+1,m-w[i])+p[i]);
-}
 int main()
 {
-int m,i,max_profit;
-printf("\nEnter the number of objects:");
+int dist[50][50],temp[50][50],n,i,j,k,x;
+printf("\nEnter the number of nodes : ");
 scanf("%d",&n);
-printf("\nEnter the knapsack capacity:");
-scanf("%d", &m);
-printf("\nEnter profit followed by weight:\n");
-for(i=1; i<=n; i++)
-scanf("%d %d", &p[i], &w[i]);
-max_profit=knap(1,m);
-printf("\nMax profit=%d", max_profit);
-return 0;
+printf("\nEnter the distance matrix :\n");
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+scanf("%d",&dist[i][j]);
+dist[i][i]=0;
+temp[i][j]=j;
+}
+printf("\n");
+}
+{
+for (i = 0; i < n; i++)
+for (j = 0; j < n; j++)
+for (k = 0; k < n; k++)
+if (dist[i][k] + dist[k][j] < dist[i][j])
+{
+dist[i][j] = dist[i][k] + dist[k][j];
+temp[i][j] = k;
+}
+for(i=0;i<n;i++)
+{
+printf("\n\nState value for router %d is \n",i+1);
+for(j=0;j<n;j++)
+printf("\t\nnode %d via %d Distance%d",j+1,temp[i][j]+1,dist[i][j]);
+}
+printf("\n\n");
+}
 }
