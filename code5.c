256da6df8f8c02982c6b0de82eb66f01d08f2420
kcstar09 <kc791534@gmail.com>
2024-12-25 21:05:25 +0530

    Update code5.c

--- code5.c
+++ code5.c
@@ -1,45 +1,280 @@
-#include<stdio.h>
-int temp[10],k=0;
-void sort(int a[][10],int id[],int n)
-{
-int i,j;
-for(i=1; i<=n; i++)
-{
-if(id[i]==0)
-{
-id[i]=-1;
-temp[++k]=i;
-for(j=1; j<=n; j++)
-{
-if(a[i][j]==1 && id[j]!=-1)
-id[j]--;
-}
-i=0;
-}
-}
-}
-void main()
-{
-int a[10][10],id[10],n,i,j;
-printf("\nEnter the n value:");
-scanf("%d",&n);
-for(i=1; i<=n; i++)
-id[i]=0;
-printf("\nEnter the graph data:\n");
-for(i=1; i<=n; i++)
-for(j=1; j<=n; j++)
-{
-scanf("%d",&a[i][j]);
-if(a[i][j]==1)
-id[j]++;
-}
-sort(a,id,n);
-if(k!=n)
-printf("\nTopological ordering not possible");
-else
-{
-printf("\nTopological ordering is:");
-for(i=1;i<=k;i++)
-printf("%d",temp[i]);
-}
-}
+<!DOCTYPE html>
+
+<head>
+
+<title>Newspaper Page </title>
+
+
+
+<link rel="stylesheet" type="text/css" href="style5.css"> 
+
+
+</head>
+
+
+
+<body>
+
+<header>
+
+
+
+<a href="#">Newspaper</a>
+
+
+
+<nav>
+
+<a href="#">Home</a>
+
+<a href="#">About</a>
+
+<a href="#">Contact</a>
+
+<a href="#">Services</a>
+
+<a href="#">Marketing</a>
+
+<a href="#">Updates</a>
+
+</nav>
+
+
+
+</header>
+
+
+
+<div class="content">
+
+<main class="main-content">
+
+<article>
+
+<h2>Article Title 1</h2>
+
+<figure>
+
+<img src="https://via.placeholder.com/600x400" alt="Placeholder Image">
+
+<figcaption>Image Caption</figcaption>
+
+</figure>
+
+<p>This is the content of the first article. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+</article>
+
+
+
+<article>
+
+<h2>Article Title 2</h2>
+
+<figure>
+
+<img src="https://via.placeholder.com/600x400" alt="Placeholder Image">
+
+<figcaption>Image Caption</figcaption>
+
+</figure>
+
+<p>This is the content of the second article. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+</article>
+
+
+
+<article>
+
+<h2>Article Title 3</h2>
+
+<figure>
+
+<img src="https://via.placeholder.com/600x400" alt="Placeholder Image">
+
+<figcaption>Image Caption</figcaption>
+
+</figure>
+
+<p>This is the content of the third article. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+</article>
+
+
+
+<article>
+
+<h2>Article Title 4</h2>
+
+<figure>
+
+<img src="https://via.placeholder.com/600x400" alt="Placeholder Image">
+
+<figcaption>Image Caption</figcaption>
+
+</figure>
+
+<p>This is the content of the fourth article. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+</article>
+
+
+
+<article>
+
+<h2>Article Title 5</h2>
+
+<figure>
+
+<img src="https://via.placeholder.com/600x400" alt="Placeholder Image">
+
+<figcaption>Image Caption</figcaption>
+
+</figure>
+
+<p>This is the content of the fourth article. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+</article>
+
+
+
+
+
+<article>
+
+<h2>Article Title 6</h2>
+
+<figure>
+
+<img src="https://via.placeholder.com/600x400" alt="Placeholder Image">
+
+<figcaption>Image Caption</figcaption>
+
+</figure>
+
+<p>This is the content of the fourth article. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+</article>
+
+
+
+</main>
+
+
+
+
+
+
+
+<aside class="related-news">
+
+<h3>Related News</h3>
+
+<ul>
+
+<li><a href="#">Related News 1</a></li>
+
+<li><a href="#">Related News 2</a></li>
+
+<li><a href="#">Related News 3</a></li>
+
+</ul>
+
+</aside>
+
+
+
+</div>
+
+
+
+<section>
+
+<h2>Recent Posts</h2>
+
+<div>
+
+<table>
+
+<caption>List of Posts</caption>
+
+<thead>
+
+<tr>
+
+<th>Post Title</th>
+
+<th>Date</th>
+
+<th>Author</th>
+
+</tr>
+
+</thead>
+
+<tbody>
+
+<tr>
+
+<td>Post 1</td>
+
+<td>2024-08-30</td>
+
+<td>Author 1</td>
+
+</tr>
+
+<tr>
+
+<td>Post 2</td>
+
+<td>2024-08-29</td>
+
+<td>Author 2</td>
+
+</tr>
+
+<tr>
+
+<td>Post 3</td>
+
+<td>2024-08-28</td>
+
+<td>Author 3</td>
+
+</tr>
+
+</tbody>
+
+</table>
+
+</div>
+
+</section>
+
+
+
+<footer>
+
+<p>Â© 2024 Newspaper. All rights reserved.</p>
+
+</footer>
+
+</body>
+
+
+
+</html>
