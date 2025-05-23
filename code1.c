d109f41afede662d901fcc49fdf9ea3da6f96e6f
kcstar09 <kc791534@gmail.com>
2025-05-24 00:27:26 +0530

    Update code1.c

--- code1.c
+++ code1.c
@@ -1,22 +1,27 @@
-app.js
-import React, { useState } from 'react';
-import './App.css';
-function App() {
-const [text, setText] = useState('');
-const handleChange = (event) => {
-setText(event.target.value);
-};
-return (
-<div className="App">
-<h1>Dynamic Text Display</h1>
-<input
-type="text"
-value={text}
-onChange={handleChange}
-placeholder="Type something..."
-/>
-<p>You typed: {text}</p>
-</div>
-);
-}
-export default App;
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_california_housing
+
+cal_housing = fetch_california_housing()
+df = pd.DataFrame(cal_housing.data,columns=cal_housing.feature_names)
+df['MedHouseVal'] = cal_housing.target
+
+
+print(df.head())
+print("\nSummary statistics:")
+print(df.describe())
+for col in df.columns:
+    plt.figure()
+    plt.hist(df[col]) 
+    plt.title(f"Histogram of {col}")
+    plt.xlabel(col)
+    plt.ylabel("Frequency")
+    plt.show()
+
+for col in df.columns:
+    plt.figure()
+    plt.boxplot(df[col], vert=True) 
+
+    plt.title(f"Box Plot of {col}")
+    plt.ylabel(col)
+    plt.show()
